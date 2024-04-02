e1ea9415978b7cb95dbbb5b71149e5677d6d9094
ibaesuyeon <ibaesuyeon@Naver.com>
2024-04-02 12:49:40 +0900

    initial commit

---
+++ src/main/java/depth/server/study/ServerStudyApplication.java
@@ -0,0 +1,13 @@
+package depth.server.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/depth/server/study/ServerStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package depth.server.study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
